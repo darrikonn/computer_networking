4602e416c63ed1c50da33a689eff840fff2f4dc1
Darri Steinn Konradsson <darri15@hotmail.com>
2016-10-02 21:11:17 +0000

    finito

--- assignments/pa2/valgrind_run.sh
+++ assignments/pa2/valgrind_run.sh
@@ -8,5 +8,5 @@ if [ -z "$1" ]
 fi
 
 make -C src/.
-env G_DEBUG=gc-friendly G_SLICE=always-malloc valgrind --leak-check=full --track-origins=yes ./src/httpd $port
+env G_DEBUG=gc-friendly G_SLICE=always-malloc valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes ./src/httpd $port
 # --show-leak-kinds=all
